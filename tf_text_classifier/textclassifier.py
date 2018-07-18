45341b69b0f506ca62e3cdc577f50a6f7a00bd44
michellebonat <michelle.bonat@gmail.com>
2018-07-18 11:58:49 -0700

    Print first movie review

--- tf_text_classifier/textclassifier.py
+++ tf_text_classifier/textclassifier.py
@@ -7,6 +7,12 @@ print(tf.__version__)
 
 # Download IMDB dataset of 50k movie reviews
 # num_words=10000 keeps only the top 10,000 most frequently occurring words in the training data
-imdbimdb  ==  keraskeras..datasetsdataset .imdb
+imdb = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+# Explore the data
+# Convert text to integers
+# print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
+# Print first movie review
+print(train_data[0])
