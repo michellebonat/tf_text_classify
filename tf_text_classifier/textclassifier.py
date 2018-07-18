b10e4762ea24faf27ee5bc7adffb1d08a187fd6c
michellebonat <michelle.bonat@gmail.com>
2018-07-18 11:35:03 -0700

    Download IMDB movie reviews dataset, keeping only most frequent words in training

--- tf_text_classifier/textclassifier.py
+++ tf_text_classifier/textclassifier.py
@@ -4,3 +4,9 @@ from tensorflow import keras
 import numpy as np
 
 print(tf.__version__)
+
+# Download IMDB dataset of 50k movie reviews
+# num_words=10000 keeps only the top 10,000 most frequently occurring words in the training data
+imdbimdb  ==  keraskeras..datasetsdataset .imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
