7872a37f0d602e11f6070d331576c67ba13ea849
michellebonat <michelle.bonat@gmail.com>
2018-07-18 21:54:42 -0700

    Download the IMDB dataset, keeping the top 10,000 most frequently occurring words in the training data

--- tf_text_classify/classify.py
+++ tf_text_classify/classify.py
@@ -3,4 +3,9 @@ from tensorflow import keras
 
 import numpy as np
 
-print(tf.__version__)
+# print(tf.__version__)
+
+# Download the IMDB dataset, keeping the top 10,000 most frequently occurring words in the training data
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
