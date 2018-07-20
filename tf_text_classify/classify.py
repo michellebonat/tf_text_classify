d182943d27af780d08e10f0d4b008b6f1efa2799
michellebonat <michelle.bonat@gmail.com>
2018-07-20 16:42:11 -0700

    Plot the training and validation loss for comparison

--- tf_text_classify/classify.py
+++ tf_text_classify/classify.py
@@ -102,6 +102,24 @@ history = model.fit(partial_x_train,
 history_dict = history.history
 history_dict.keys()
 
-dict_keys(['loss', 'acc', 'val_acc', 'val_loss'])
+# Plot the training and validation loss for comparison, as well as the training and validation accuracy
+import matplotlib.pyplot as plt
 
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
 
