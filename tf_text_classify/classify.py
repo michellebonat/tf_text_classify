e50e5a60b0c90cce0f3d77e6689d0ff7a05cea2b
michellebonat <michelle.bonat@gmail.com>
2018-07-20 17:46:40 -0700

    Plot training and validation accuracy

--- tf_text_classify/classify.py
+++ tf_text_classify/classify.py
@@ -112,14 +112,28 @@ val_loss = history.history['val_loss']
 
 epochs = range(1, len(acc) + 1)
 
+# Plot training and validation loss
 # "bo" is for "blue dot"
-plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, loss, 'bo', label='Training loss')
 # b is for "solid blue line"
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+
+# plt.show()
+
+# Plot training and validation accuracy
+plt.clf()   # clear figure
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
-plt.ylabel('Loss')
+plt.ylabel('Accuracy')
 plt.legend()
 
 plt.show()
-
