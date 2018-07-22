69f057066a95e11306e05f509926cff11d97542e
michellebonat <michelle.bonat@gmail.com>
2018-07-21 19:03:22 -0700

    First commit, setup env and modules with Anaconda, ScikitLearn, Numpy, Pandas, with RFR and pipeline, and some metrics

---
+++ ds_classify_3/classify.py
@@ -0,0 +1,11 @@
+# 1) Import libraries and modules
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.externals import joblib
