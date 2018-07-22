97ac330a33862479bfe5656a1e64a4f4304006fd
michellebonat <michelle.bonat@gmail.com>
2018-07-21 19:08:03 -0700

    Loads data from the DS public store and print the first 5 rows

--- ds_classify_3/classify.py
+++ ds_classify_3/classify.py
@@ -9,3 +9,11 @@ from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.externals import joblib
+
+# 2) Load data from remote url
+
+dataset_url = 'https://s3.amazonaws.com/worldquant-dropbox/datasimply_export.csv'
+data = pd.read_csv(dataset_url)
+
+# Uncomment this to print first 5 rows of data
+print data.head()
